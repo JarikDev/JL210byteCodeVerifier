0591dc13e4905f75381a5a12d3f626ed7b0b89df
ivan4000 <sxln1@mail.ru>
2018-11-20 01:46:59 +0300

    JL210byteCodeVerifyer

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2 + 3 == " + fun());
+    }
+
+    public static int fun() {
+        int integerOne;
+        int integerTwo;
+        integerOne = 2;
+        integerTwo = 3;
+        int result = integerOne + integerTwo;
+        return result;
+    }
+}
